a0bd5d8cf847b1a38e929b19dc0652cdaed65459
Natalia <sokolowskan16@gmail.com>
2020-08-24 13:12:03 +0200

    done task1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -15,6 +13,25 @@ public class Main {
             {
                 list.add(lineScanner.nextInt());
             }
+
+            int count = list.size();
+            Set<Integer> distList = new HashSet<>(list);
+            int distinct = distList.size();
+            list = new ArrayList<>(distList);
+            Collections.sort(list);
+            int min = list.get(0);
+            int max = list.get(list.size()-1);
+
+
+            list.forEach((ele) -> {
+                System.out.print(ele + " ");
+            });
+            System.out.println();
+
+            System.out.println("count: " + count);
+            System.out.println("distinct: " + distinct);
+            System.out.println("min: " + min);
+            System.out.println("max: " + max);
         }
     }
 }
